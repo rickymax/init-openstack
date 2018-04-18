5f0abfb1ed3282ea2bb3e5b0d55882e4b9e67fb8
rickymax <rickyomax@gmail.com>
2018-04-18 09:12:06 +0000

    modify flavor

--- create-flavor.sh
+++ create-flavor.sh
@@ -1,18 +1,23 @@
 openstack flavor create minimal-lowmem --public --id 10 \
-    --vcpus 1 --ram 2048 --disk 30 &&
+    --vcpus 1 --ram 2048 --disk 3 --ephemeral 10 &&
 openstack flavor create minimal --public --id 11 \
-    --vcpus 1 --ram 4096 --disk 30 &&
+    --vcpus 1 --ram 4096 --disk 3 --ephemeral 10 &&
 
 openstack flavor create small-lowmem --public --id 13 \
-    --vcpus 2 --ram 4096 --disk 30 &&
+    --vcpus 2 --ram 4096 --disk 3 --ephemeral 14 &&
 openstack flavor create small --public --id 14 \
-    --vcpus 2 --ram 8192 --disk 30 &&
+    --vcpus 2 --ram 8192 --disk 3 --ephemeral 14 &&
 openstack flavor create small-highmem --public --id 15 \
-    --vcpus 2 --ram 16384 --disk 30 &&
+    --vcpus 2 --ram 16384 --disk 3 --ephemeral 14 &&
 
 openstack flavor create medium-lowmem --public --id 16 \
-    --vcpus 4 --ram 8192 --disk 30 &&
+    --vcpus 4 --ram 8192 --disk 3 --ephemeral 18 &&
 openstack flavor create medium --public --id 17 \
-    --vcpus 4 --ram 16384 --disk 30 &&
+    --vcpus 4 --ram 16384 --disk 3 --ephemeral 18 &&
 openstack flavor create medium-highmem --public --id 18 \
-    --vcpus 4 --ram 32768 --disk 100 
+    --vcpus 4 --ram 32768 --disk 3 --ephemeral 18 &&
+
+openstack flavor create small-50GB-ephemeral-disk --public --id 20 \
+    --vcpus 2 --ram 8192 --disk 3 --ephemeral 50 &&
+openstack flavor create medium-highmem-100GB-ephemeral-disk --public --id 21 \
+    --vcpus 4 --ram 32768 --disk 3 --ephemeral 100
