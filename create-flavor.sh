1b843e84c2509a66b4ef347d24a9e9c40b1112e0
rickymax <rickyomax@gmail.com>
2018-08-08 03:16:27 +0000

    use twice the amount of RAM as ephemeral disk size

--- create-flavor.sh
+++ create-flavor.sh
@@ -4,18 +4,18 @@ openstack flavor create minimal --public --id 11 \
     --vcpus 1 --ram 4096 --disk 3 --ephemeral 10 &&
 
 openstack flavor create small-lowmem --public --id 13 \
-    --vcpus 2 --ram 4096 --disk 3 --ephemeral 14 &&
+    --vcpus 2 --ram 4096 --disk 3 --ephemeral 10 &&
 openstack flavor create small --public --id 14 \
-    --vcpus 2 --ram 8192 --disk 3 --ephemeral 14 &&
+    --vcpus 2 --ram 8192 --disk 3 --ephemeral 16 &&
 openstack flavor create small-highmem --public --id 15 \
-    --vcpus 2 --ram 16384 --disk 3 --ephemeral 14 &&
+    --vcpus 2 --ram 16384 --disk 3 --ephemeral 32 &&
 
 openstack flavor create medium-lowmem --public --id 16 \
-    --vcpus 4 --ram 8192 --disk 3 --ephemeral 18 &&
+    --vcpus 4 --ram 8192 --disk 3 --ephemeral 16 &&
 openstack flavor create medium --public --id 17 \
-    --vcpus 4 --ram 16384 --disk 3 --ephemeral 18 &&
+    --vcpus 4 --ram 16384 --disk 3 --ephemeral 32 &&
 openstack flavor create medium-highmem --public --id 18 \
-    --vcpus 4 --ram 32768 --disk 3 --ephemeral 18 &&
+    --vcpus 4 --ram 32768 --disk 3 --ephemeral 64 &&
 
 openstack flavor create small-50GB-ephemeral-disk --public --id 20 \
     --vcpus 2 --ram 8192 --disk 3 --ephemeral 50 &&
